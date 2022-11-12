6a2bd2593893d5c6ddd2522cfb91510cf464f8ca
Hari Srinivasan <59945818+HariSrinivasan14@users.noreply.github.com>
2022-11-12 18:24:47 -0500

    added non-recursive version of Binary Search

--- SearchAlgorithms.py
+++ SearchAlgorithms.py
@@ -17,8 +17,24 @@ def binarySearch(arr, num, minIndex=-1, maxIndex=-1):
     elif num > arr[middleIndex]: # search in the right subarray
         return binarySearch(arr, num, middleIndex + 1, len(arr) - 1)
     else: # search in the left subarray
-        return binarySearch(arr, num, minIndex, middleIndex - 1)  
+        return binarySearch(arr, num, minIndex, middleIndex - 1)
 
+
+def binarySearchNonRecursive(arr, target):
+    left = 0 
+    right = len(nums) - 1
+
+    while left <= right:
+        midIndex = left + (right - left) // 2
+        if nums[midIndex] == target:
+            return midIndex
+        elif nums[midIndex] < target: # search right
+            left = midIndex + 1
+        else: #search left
+            right = midIndex - 1
+    return -1 # could not find target, return -1
+    
+    
 def main():
     num = 15
     arr = [1,2 ,3, 4, 15]
